4df3937074299b44439dde5591b65bac91767bad
David Dixon [Sandwich] <dd331@exeter.ac.uk>
2018-10-29 14:58:50 +0000

    Init dump

---
+++ aco.py
@@ -0,0 +1,134 @@
+import random
+import matplotlib.pyplot as plt
+
+import time
+
+k = 100 # Number of Weights
+b = 10 # Bin Number
+p = 10 # Ant Number
+e = 0.4 # Evaporation Rate
+t = 1000 # Max Number of Iterations
+
+class Weight:
+    def __init__(self, value):
+        self.value = value
+        self.pheromones = [random.uniform(0,1)] * b
+
+    def getValue(self):
+        return self.value
+
+    def getPheromones(self):
+        return self.pheromones
+    
+    def chooseRandBin(self):
+        x = sum(self.pheromones)
+        r = random.uniform(0,x)
+        s = 0
+        for i in range(b):
+            s += self.pheromones[i]
+            if s >= r:
+                return i
+        return b - 1
+
+    def evaporate(self):
+        for phero in self.pheromones:
+            phero *= e
+    
+    def increase(self, path, value):
+        self.pheromones[path] += value
+
+
+class Ant:
+    def __init__(self):
+        self.path = [0] * k
+        self.binValue = [0] * b
+
+    def generatePath(self, weights):
+        self.binValue = [0] * b
+        for i in range(k):
+            x = weights[i].chooseRandBin()
+            self.path[i] = x
+            self.binValue[x] += weights[i].getValue()
+
+    def fitness(self):
+        return max(self.binValue) - min(self.binValue)
+
+    def updatePheromone(self, weights):
+        for i in range(k):
+            x = self.fitness()
+            if x == 0:
+                weights[i].increase(self.path[i], float("inf"))
+            else:
+                weights[i].increase(self.path[i], 10000 / x**2)
+                #weights[i].increase(self.path[i], 100 / x)
+
+
+def findBinsOfPath(path, weights):
+    bins = [0]*b
+    for i in range(k):
+        bins[path[i]] += weights[i].getValue()
+    return bins
+
+
+start = time.clock()
+
+
+weights = []
+
+#BPP1
+""" for i in range(k):
+    x  = Weight(random.randint(1,201))
+    weights.append(x) """
+
+#BPP2
+for i in range(k):
+    x  = Weight((random.randint(1,201) * i)/2)
+    weights.append(x)
+
+ants = []
+
+for i in range(p):
+    x = Ant()
+    ants.append(x)
+
+minFit = float("inf")
+minPath = []
+
+fitnesses = []
+
+for i in range(t):
+    genFit = []
+    for ant in ants:
+        ant.generatePath(weights)
+        x = ant.fitness()
+        genFit.append(x)
+        if x < minFit:
+            minFit = x
+            minPath = ant.path.copy()
+            print(i)
+            print(minFit)
+            print(findBinsOfPath(minPath,weights))
+    for ant in ants:
+        ant.updatePheromone(weights)
+    for weight in weights:
+        weight.evaporate()
+    fitnesses.append(genFit)
+    if minFit == 0:
+        break
+
+
+
+print(time.clock() - start)
+
+out = []
+
+minfit = float("inf")
+for x in fitnesses:
+    if min(x) < minfit:
+        minfit = min(x)
+    out.append([minfit,max(x),(sum(x)/p)])
+
+print("Out")
+
+plt.plot(out)
+plt.show()
